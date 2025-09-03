3a559d32d309aecd52ebc3f25734900574c2fd44
Thalles Bruno Santos <thallesbgns@gmail.com>
2025-09-03 17:57:18 -0300

    Adicionando Fila.java.

---
+++ src/main/java/Fila.java
@@ -0,0 +1,21 @@
+public class Fila implements IEstruturaDeDados{
+    @Override
+    public void adicionar(int valor) {
+
+    }
+
+    @Override
+    public int remover() {
+        return 0;
+    }
+
+    @Override
+    public int tamanho() {
+        return 0;
+    }
+
+    @Override
+    public boolean estaVazia() {
+        return false;
+    }
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,8 +8,8 @@ public class Main {
         pilha.adicionar(20);
         pilha.adicionar(30);
         pilha.adicionar(40);
-        pilha.adicionar(50);
-        pilha.adicionar(60);
+        /*pilha.adicionar(50);
+        pilha.adicionar(60);*/
 
         System.out.println("Topo da pilha: " + pilha.peek()); // 30
         System.out.println("Tamanho da pilha: " + pilha.tamanho()); // 3
